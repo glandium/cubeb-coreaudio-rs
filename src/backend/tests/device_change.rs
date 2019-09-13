f862dbd8cb29174c294daf85c1074e806d1798f2
Chun-Min Chang <chun.m.chang@gmail.com>
2019-09-13 11:33:07 -0700

    Print devices before switching them

--- src/backend/tests/device_change.rs
+++ src/backend/tests/device_change.rs
@@ -16,8 +16,8 @@
 
 use super::utils::{
     test_create_device_change_listener, test_device_in_scope, test_get_default_device,
-    test_get_devices_in_scope, test_ops_stream_operation, test_set_default_device, Scope,
-    TestDevicePlugger, TestDeviceSwitcher,
+    test_get_devices_in_scope, test_ops_stream_operation, test_print_devices_in_scope,
+    test_set_default_device, Scope, TestDevicePlugger, TestDeviceSwitcher,
 };
 use super::*;
 use std::fmt::Debug;
@@ -31,6 +31,8 @@ fn test_switch_device() {
 }
 
 fn test_switch_device_in_scope(scope: Scope) {
+    test_print_devices_in_scope(scope.clone());
+
     // Do nothing if there is no 2 available devices at least.
     let devices = test_get_devices_in_scope(scope.clone());
     if devices.len() < 2 {
